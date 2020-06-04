67e8b805c6858da3492d5467bff9d2f4d5d70959
GITHUBear <1729307751@qq.com>
2020-06-04 17:25:38 +0800

    add info log for debug in flush_engine_properties
    Signed-off-by: GITHUBear <1729307751@qq.com>

--- components/engine_rocks/src/rocks_metrics.rs
+++ components/engine_rocks/src/rocks_metrics.rs
@@ -856,6 +856,11 @@ pub fn flush_engine_properties(engine: &DB, name: &str, shared_block_cache: bool
         let handle = crate::util::get_cf_handle(engine, cf).unwrap();
         // It is important to monitor each cf's size, especially the "raft" and "lock" column
         // families.
+        let cf_used_size = crate::util::get_engine_cf_used_size(engine, handle);
+        STORE_ENGINE_SIZE_GAUGE_VEC
+            .with_label_values(&[name, cf])
+            .set(cf_used_size as i64);
+
         if !shared_block_cache {
             let block_cache_usage = engine.get_block_cache_usage_cf(handle);
             STORE_ENGINE_BLOCK_CACHE_USAGE_GAUGE_VEC
@@ -863,13 +868,16 @@ pub fn flush_engine_properties(engine: &DB, name: &str, shared_block_cache: bool
                 .set(block_cache_usage as i64);
         }
 
+        info!("============== PATHS_SIZE_PROPERTY ==========");
         if let Some(path_size_map) = engine.get_map_property_cf(handle, ROCKSDB_PATHS_SIZE) {
             for i in 0..engine.get_db_options().get_db_paths_num() {
-                STORE_ENGINE_SIZE_GAUGE_VEC
+                info!("path {} : {}", i, path_size_map.get_property_int_value(&i.to_string()));
+                STORE_ENGINE_PATH_SIZE_GAUGE_VEC
                     .with_label_values(&[name, cf, &i.to_string()])
                     .set(path_size_map.get_property_int_value(&i.to_string()) as i64);
             }
         }
+        info!("=============================================");
 
         // TODO: find a better place to record these metrics.
         // Refer: https://github.com/facebook/rocksdb/wiki/Memory-usage-in-RocksDB
@@ -1038,6 +1046,11 @@ lazy_static! {
     pub static ref STORE_ENGINE_SIZE_GAUGE_VEC: IntGaugeVec = register_int_gauge_vec!(
         "tikv_engine_size_bytes",
         "Sizes of each column families",
+        &["db", "type"]
+    ).unwrap();
+    pub static ref STORE_ENGINE_PATH_SIZE_GAUGE_VEC: IntGaugeVec = register_int_gauge_vec!(
+        "tikv_engine_path_size_bytes",
+        "Sizes of each column families and path",
         &["db", "type", "path_id"]
     ).unwrap();
     pub static ref STORE_ENGINE_BLOCK_CACHE_USAGE_GAUGE_VEC: IntGaugeVec = register_int_gauge_vec!(
